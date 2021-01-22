8e696515b2773b1eb5c95841450661a2bff7689a
Marcus Ho <marcusho1993@gmail.com>
2021-01-22 16:27:36 -0500

    Product Detail Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,23 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 import Header from './components/Header'
 import Footer from './components/Footer'
 import { Container } from 'react-bootstrap'
 import HomeScreen from './Screens/HomeScreen'
+import ProductScreen from './Screens/ProductScreen'
 
 function App() {
 	return (
-		<>
+		<Router>
 			<Header />
 			<main className='py-3'>
 				<Container>
-					<HomeScreen />
+					<Route path='/' component={HomeScreen} exact />
+					<Route path='/product/:id' component={ProductScreen} />
 				</Container>
 			</main>
 			<Footer />
-		</>
+		</Router>
 	)
 }
 
