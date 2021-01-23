f2eb2d4298d6d1b90d3ec77b3300880c86cfaf18
Marcus Ho <marcusho1993@gmail.com>
2021-01-23 15:37:10 -0500

    Add custom ErrorHandlers to backend

--- backend/server.js
+++ backend/server.js
@@ -2,21 +2,30 @@ import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 import productRoutes from './routes/productRoutes.js'
 
+// dotenv setup
 dotenv.config()
 
+// DB Connector
 connectDB()
 
+// Init express app
 const app = express()
 
+// Routes setup
 app.get('/', (req, res) => {
 	res.send('API is running')
 })
-
 app.use('/api/products', productRoutes)
 
+// Route ExceptionHandlers
+app.use(notFound)
+app.use(errorHandler)
+
+// Connection Listener
 const PORT = process.env.PORT || 5000
 const NODE_ENV = process.env.NODE_ENV
 
