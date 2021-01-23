d47ef9139bd60b14187b9f3927d8042941867ddd
Marcus Ho <marcusho1993@gmail.com>
2021-01-22 23:23:13 -0500

    Add colors to console msg

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import colors from 'colors'
 import connectDB from './config/db.js'
 import products from './data/products.js'
 
@@ -27,5 +28,5 @@ const NODE_ENV = process.env.NODE_ENV
 
 app.listen(
 	PORT,
-	console.log(`Server running in ${NODE_ENV} mode on port ${PORT}`)
+	console.log(`Server running in ${NODE_ENV} mode on port ${PORT}`.yellow.bold)
 )
