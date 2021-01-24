34c30b515d37aa28376699a8f0870fe41df05138
Marcus Ho <marcusho1993@gmail.com>
2021-01-23 22:37:29 -0500

    Add to cart function and Qty select in ProductScreen

--- frontend/src/Screens/ProductScreen.js
+++ frontend/src/Screens/ProductScreen.js
@@ -1,20 +1,26 @@
-import { useEffect } from 'react'
-import { Link, useParams } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { Link, useParams, useHistory } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
-import { Col, Row, Image, ListGroup, Card, Button } from 'react-bootstrap'
+import { Col, Row, Image, ListGroup, Card, Button, Form } from 'react-bootstrap'
 import Rating from '../components/Rating'
 import Loader from '../components/Loader'
 import Message from '../components/Message'
 import { listProductDetails } from '../redux/actions/productActions'
 
 const ProductScreen = () => {
+	const [qty, setQty] = useState(0)
 	const dispatch = useDispatch()
 	const { loading, error, product } = useSelector(state => state.productDetails)
 	const { id } = useParams()
+	const { push } = useHistory()
 
 	useEffect(() => {
 		dispatch(listProductDetails(id))
 	}, [dispatch, id])
+
+	const addToCartHandler = () => {
+		push(`/cart/${id}?qty=${qty}`)
+	}
 
 	return (
 		<>
@@ -66,8 +72,30 @@ const ProductScreen = () => {
 										</Col>
 									</Row>
 								</ListGroup.Item>
+
+								{product.countInStock > 0 && (
+									<ListGroup.Item>
+										<Row>
+											<Col>Qty</Col>
+											<Col>
+												<Form.Control
+													as='select'
+													value={qty}
+													onChange={e => setQty(e.target.value)}>
+													{[...Array(product.countInStock).keys()].map(x => (
+														<option key={x + 1} value={x + 1}>
+															{x + 1}
+														</option>
+													))}
+												</Form.Control>
+											</Col>
+										</Row>
+									</ListGroup.Item>
+								)}
+
 								<ListGroup.Item>
 									<Button
+										onClick={addToCartHandler}
 										className='btn-block'
 										type='button'
 										disabled={product.countInStock === 0}>
