d58fa919e345d26b6056c81cf316fab4fca8a2ad
Marcus Ho <marcusho1993@gmail.com>
2021-01-23 22:00:11 -0500

    use redux to fetch products data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,8 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import { Container } from 'react-bootstrap'
-import HomeScreen from './Screens/HomeScreen'
-import ProductScreen from './Screens/ProductScreen'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
 function App() {
 	return (
--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -1,30 +1,33 @@
-import { useState, useEffect } from 'react'
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Col, Row } from 'react-bootstrap'
-import axios from 'axios'
 import Product from '../components/Product'
+import { listProducts } from '../redux/actions/productActions'
 
 const HomeScreen = () => {
-	const [products, setProducts] = useState([])
+	const dispatch = useDispatch()
+	const { loading, error, products } = useSelector(state => state.productList)
 
 	useEffect(() => {
-		const fetchProducts = async () => {
-			const { data } = await axios.get('/api/products')
-			setProducts(data)
-		}
-
-		fetchProducts()
-	}, [])
+		dispatch(listProducts())
+	}, [dispatch])
 
 	return (
 		<>
 			<h1>Latest Products</h1>
-			<Row>
-				{products.map(product => (
-					<Col sm={12} md={6} lg={4} xl={3} key={product._id}>
-						<Product product={product} />
-					</Col>
-				))}
-			</Row>
+			{loading ? (
+				<h2>Loading...</h2>
+			) : error ? (
+				<h3>{error}</h3>
+			) : (
+				<Row>
+					{products.map(product => (
+						<Col sm={12} md={6} lg={4} xl={3} key={product._id}>
+							<Product product={product} />
+						</Col>
+					))}
+				</Row>
+			)}
 		</>
 	)
 }
