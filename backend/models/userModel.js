149da94e7884fe924367a125a7467a49ed04e608
Marcus Ho <marcusho1993@gmail.com>
2021-01-24 12:27:29 -0500

    set up user login route and get user data from db

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import bcrypt from 'bcryptjs'
 
 const userSchema = mongoose.Schema(
 	{
@@ -9,6 +10,11 @@ const userSchema = mongoose.Schema(
 	},
 	{ timestamps: true }
 )
+
+// !arrow function is NOT supported
+userSchema.methods.matchPassword = async function (enteredPassword) {
+	return await bcrypt.compare(enteredPassword, this.password)
+}
 
 const User = mongoose.model('User', userSchema)
 
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import connectDB from './config/db.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
 // dotenv setup
 dotenv.config()
@@ -15,11 +16,15 @@ connectDB()
 // Init express app
 const app = express()
 
+// Parse request body to JSON
+app.use(express.json())
+
 // Routes setup
 app.get('/', (req, res) => {
 	res.send('API is running')
 })
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 // Route ExceptionHandlers
 app.use(notFound)
