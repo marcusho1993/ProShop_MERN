0c395b5ff05c57256c60cccd71d09c02bcbb29b6
Marcus Ho <marcusho1993@gmail.com>
2021-01-25 00:57:54 -0500

    Create Order but not paid yet

--- frontend/src/Screens/ShippingScreen.js
+++ frontend/src/Screens/ShippingScreen.js
@@ -3,8 +3,8 @@ import { useDispatch, useSelector } from 'react-redux'
 import { useHistory } from 'react-router-dom'
 import { Form, Button } from 'react-bootstrap'
 import FormContainer from '../components/FormContainer'
-import { saveShippingAddress } from '../redux/actions/cartActions'
 import CheckoutSteps from '../components/CheckoutSteps'
+import { saveShippingAddress } from '../redux/actions/cartActions'
 
 const ShippingScreen = () => {
 	const { shippingAddress } = useSelector(state => state.cart)
